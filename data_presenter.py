1d2722dfa190cb22a0e128850800f509cc22cbed
Denys Iaremenko <denysiaremenko@Denyss-MacBook-Pro.local>
2020-07-23 17:58:24 +0300

    feat(): dou data

--- data_presenter.py
+++ data_presenter.py
@@ -1,43 +1,76 @@
 import numpy as np
+import pandas as pd
+import random
+
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, output_file, show
-import pandas as pd
-
 from datetime import datetime
 
 
 
 def create_plot(name):
     p = figure(x_axis_type="datetime", title=name)
-    p.grid.grid_line_alpha = 0.5
-    p.width = 2000
-    p.height = 800
+    p.border_fill_color = "whitesmoke"
+    p.grid.grid_line_alpha = 0.3
+    p.width = 1600
+    p.height = 1000
     p.xaxis.axis_label = 'Date'
     p.yaxis.axis_label = 'Price'
     return p
 
-def create_chart_nbu():
-    data_to_show = pd.read_csv("nbu.csv")
 
-    def datetime(x):
+def create_chart_nbu():
+    data_to_show_nbu = pd.read_csv("nbu.csv")
+    data_to_show_ukrsib = pd.read_csv("ukrsib.csv")
+    data_to_show_dou = pd.read_csv("dou.csv")
+
+    def datetime_personal(x):
         return np.array(x, dtype=np.datetime64)
 
     # NBU
     p1 = create_plot("NBU")
-    p1.line(datetime(list(data_to_show["h_t"])), list(data_to_show["e"]),
+    p1.line(datetime_personal(list(data_to_show_nbu["h_t"])), list(data_to_show_nbu["e"]),
             color='#A6CEE3', line_width=3, legend_label='Euro')
-    p1.line(datetime(list(data_to_show["h_t"])), list(data_to_show["d"]),
+    p1.line(datetime_personal(list(data_to_show_nbu["h_t"])), list(data_to_show_nbu["d"]),
             color='#B2DF8A', line_width=3, legend_label='Dollar')
+    p1.legend.location = "top_left"
+    p1.legend.click_policy = "hide"
 
     # UKRSIB
     p2 = create_plot("Ukrsib")
-    p2.line(datetime(list(data_to_show["h_t"])), list(data_to_show["e"]),
-            color='#A6CEE3', line_width=3, legend_label='Euro')
-    p2.line(datetime(list(data_to_show["h_t"])), list(data_to_show["d"]),
-            color='#B2DF8A', line_width=3, legend_label='Dollar')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["e_b"]),
+            color='yellow', line_width=3, legend_label='Euro Buy')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["e_s"]),
+            color='yellow', line_dash="dashed", line_width=3, legend_label='Euro Sell')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["d_b"]),
+            color='green', line_width=3, legend_label='Dollar Buy')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["d_s"]),
+            color='green', line_dash="dashed", line_width=3, legend_label='Dollar Sell')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["e_card_s"]),
+            color='red', line_width=3, legend_label='Euro Card Buy')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["e_card_b"]),
+            color='red', line_dash="dashed", line_width=3, legend_label='Euro Card Sell')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["d_card_s"]),
+            color='brown', line_width=3, legend_label='Dollar Card Buy')
+    p2.line(datetime_personal(list(data_to_show_ukrsib["h_t"])), list(data_to_show_ukrsib["d_card_b"]),
+            color='brown', line_dash="dashed", line_width=3, legend_label='Dollar Card Sell')
+    p2.legend.location = "top_left"
+    p2.legend.click_policy = "hide"
 
+    # DOU
+    def random_color():
+        return tuple(np.random.randint(256, size=3))
 
-    output_file("nbu.html")
-    show(gridplot([[p1], [p2]]))
+    p3 = create_plot("Dou")
+    p3.yaxis.axis_label = "Number"
+    for item in data_to_show_dou.columns.values.tolist()[2:]:
+        p3.line(datetime_personal(list(data_to_show_dou["h_t"])), list(data_to_show_dou[item]),
+                color=random_color(), line_width=3, legend_label=item)
+    p3.legend.location = "top_left"
+    p3.legend.click_policy = "hide"
+
+    output_file("current_data.html")
+    show(gridplot([[p1], [p2], [p3]]))
+
 
 create_chart_nbu()
